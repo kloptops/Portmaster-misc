a77b6e747d68e59ea661e2b6b5fab17f7182c1b1
Jacob <kloptops@gmail.com>
2023-02-25 17:43:39 +0800

    New and improved `build_zips.py` - Enable and disable zips so they wont be built unless you want them to be - Build a particular zip - Added help! - List available ports and whether they're disabled. - Made a template!
    Added small TODO list, its not that important.

---
+++ TODO.md
@@ -0,0 +1,12 @@
+# Todo lists
+
+## build_zips.py 
+
+- [x] Building only selected projects
+- [x] Enabling and disabling zips
+- [ ] Templates for new ports 
+
+## Portmaster
+
+- [ ] Update Half-Life once it is tested
+- [ ] Update OpenRCT2 when its worthwhile
--- build_zips.py
+++ build_zips.py
@@ -9,12 +9,26 @@ The zip files are only updated if the files are changed or renamed.
 """
 import hashlib
 import json
+import re
+import sys
+import textwrap
 import zipfile
-from pathlib import Path
+
 from difflib import Differ
+from pathlib import Path
 
 
-VERBOSE=False
+def name_clean(name):
+    return re.sub(r'[^a-zA-Z]+', '', name.replace('.zip', '')).lower()
+
+def name_match(name, all_names):
+    name_cleaned = name_clean(name)
+    for a_name in all_names:
+        if name_cleaned == name_clean(a_name):
+            return a_name
+
+    return None
+
 
 def build_files(root, dest_path, max_depth=None):
     stack = [root]
@@ -47,8 +61,6 @@ def build_zip(base_zip_name, root_path, paths, config):
     check_name = config['CHECK_DIR'] / (base_zip_name.rsplit('.', 1)[0] + '.sha256')
     shasum_name = config['RELEASE_DIR'] / (base_zip_name + '.sha256sum')
 
-    print(f'Checking {str(zip_name)}')
-
     # Build up a list of all files needed for the zip, and their location on the file system
     all_files = []
     for path in paths:
@@ -59,7 +71,7 @@ def build_zip(base_zip_name, root_path, paths, config):
             elif len(path) == 3:
                 real_name, internal_name, max_depth = path
             else:
-                print(f"- Bad path in paths, length of tuple must be 2 or 3: {path}, failing to build zip.")
+                print(f"  - Bad path in paths, length of tuple must be 2 or 3: {path}, failing to build zip.")
                 return
         else:
             real_name = internal_name = path
@@ -71,7 +83,7 @@ def build_zip(base_zip_name, root_path, paths, config):
             for src, dest in build_files(full_name, internal_name, max_depth):
                 all_files.append((str(dest), str(src)))
         else:
-            print(f"- {str(full_name)!r} failing to build zip.")
+            print(f"  - {str(full_name)!r} failing to build zip.")
             return
 
 
@@ -112,7 +124,7 @@ def build_zip(base_zip_name, root_path, paths, config):
 
             # See if the files have changed.
             if new_digest == old_digest:
-                print('- File is up to date.')
+                print('  - File is up to date.')
                 return
             else:
                 # Build up what files have changed
@@ -133,16 +145,16 @@ def build_zip(base_zip_name, root_path, paths, config):
                             # File is just added.
                             changes[name] = 'Added'
 
-                print("- Files changed")
+                print("  - Files changed")
                 for name, mode in changes.items():
-                    print(f"  - {mode} {name}")
+                    print(f"    - {mode} {name}")
 
-    print('- Building Zip')
+    print('  - Building Zip')
     with zipfile.ZipFile(zip_name, 'w', compression=zipfile.ZIP_DEFLATED, compresslevel=9) as zf:
         for file_pair in all_files:
             zf.write(file_pair[1], file_pair[0])
 
-    print('- Recording sha256sum')
+    print('  - Recording sha256sum')
     final_hash = hashlib.sha256()
     with open(zip_name, 'rb') as fh:
         final_hash.update(fh.read())
@@ -152,29 +164,227 @@ def build_zip(base_zip_name, root_path, paths, config):
     with open(shasum_name, 'w') as fh:
         fh.write(f'{final_digest} *{Path(zip_name).name}')
 
-    print('- Recording Checkfile')
+    print('  - Recording Checkfile')
     with open(check_name, 'w') as fh:
         fh.write(''.join(all_digests))
 
 
-config = {
-    'RELEASE_DIR': Path('releases'),
-    'CHECK_DIR': Path('.releases'),
+def do_build(config, build_configs, args):
+    """
+    Builds a zip file based on build_zip.json, usage:
+
+    {command} build all
+    {command} build <zip name>
+    """
+
+    if len(args) == 0:
+        args.append('all')
+
+    if args[0] == 'all':
+        print("Building all zips:")
+
+        for zip_name, zip_config in build_configs.items():
+            print(f"- {zip_name}")
+            if zip_config['disabled'].is_file():
+                print("  - disabled skipping")
+                continue
+
+            build_zip(
+                zip_config['zip_file'],
+                zip_config['directory'],
+                zip_config['files'],
+                config)
+    else:
+        print("Building selected zips:")
+
+        for name in args:
+            match_name = name_match(name, build_configs.keys())
+            if match_name is None:
+                print(f"- Unknown zip {name!r}")
+                continue
+
+            print(f"- Building {build_configs[match_name]['zip_file']}")
+            
+            build_zip(
+                build_configs[match_name]['zip_file'],
+                build_configs[match_name]['directory'],
+                build_configs[match_name]['files'],
+                config)
+
+
+def do_enable(config, build_configs, args):
+    """
+    Enables a port being build by "build all"
+
+    {command} enable all
+    {command} enable <zip name>
+    """
+    if len(args) == 0:
+        print("No <zip name> specified, see help for usage")
+        do_help(config, build_configs, ['enable'])
+        return
+
+    enable_count = 0
+    if args[0] == 'all':
+        print("Enabling all zips:")
+
+        for zip_name, zip_config in build_configs.items():
+            if zip_config['disabled'].is_file():
+                zip_config['disabled'].unlink()
+                print(f"- {zip_config['zip_file']} enabled")
+                enable_count += 1
+
+        print(f"- Enabled {enable_count} / {len(build_configs)} items")
+
+    else:
+        print("Enabling selected zips:")
+
+        for name in args:
+            match_name = name_match(name, build_configs.keys())
+            if match_name is None:
+                print(f"- Unknown zip {name!r}, skipping")
+                continue
+
+            if build_configs[match_name]['disabled'].is_file():
+                build_configs[match_name]['disabled'].unlink()
+                print(f"- {build_configs[match_name]['zip_file']} enabled")
+                enable_count += 1
+
+        print(f"- Enabled {enable_count} / {len(args)} items")
+
+
+def do_disable(config, build_configs, args):
+    """
+    Disable a port being build by "build all", it will still be built by "{command} build <zip name>"
+
+    The zip is disabled by making a file named ".build_disable" in the directory with "build_zip.json"
+
+    {command} disable all
+    {command} disable <zip name>[ <zip name>]+
+    """
+
+    disable_count = 0
+    if args[0] == 'all':
+        print("Disabling all zips:")
+
+        for zip_name, zip_config in build_configs.items():
+            if not zip_config['disabled'].is_file():
+                zip_config['disabled'].touch()
+                print(f"- {zip_config['zip_file']} disabled")
+                disable_count += 1
+
+        print(f"- Disabled {disable_count} / {len(build_configs)} items")
+
+    else:
+        print("Disabling selected zips:")
+
+        for name in args:
+            match_name = name_match(name, build_configs.keys())
+            if match_name is None:
+                print(f"- Unknown zip {name!r}, skipping")
+                continue
+
+            if not build_configs[match_name]['disabled'].is_file():
+                build_configs[match_name]['disabled'].touch()
+                print(f"- {build_configs[match_name]['zip_file']} disabled")
+                disable_count += 1
+
+        print(f"- Enabled {disable_count} / {len(args)} items")
+
+
+def do_list(config, build_configs, args):
+    """
+    Lists all ports, and whether they're enabled or not.
+
+    {command} list
+    """
+
+    print("Available zips:")
+    for zip_name, zip_config in build_configs.items():
+        print(f"- {zip_config['zip_file']:15s} - {zip_config['disabled'].is_file() and 'Disabled' or 'Enabled'}")
+
+
+def do_help(config, build_configs, args):
+    """
+    Shows general help or help for a particular command.
+
+    {command} help
+    {command} help list
+    """
+    command = sys.argv[0]
+
+    if len(args) > 0:
+        if args[0].lower() not in all_commands:
+            print(f"Error: unknown help command {args[0]!r}")
+            do_help(config, build_configs, [])
+            return
+
+        print(textwrap.dedent(all_commands[args[0].lower()].__doc__.format(command=command)).strip())
+        return
+
+    print(f"{command} <build|enable|disable|list> <all|PORT1> [.. PORT2]")
+    print(f"{command} <help> <command>")
+    print()
+    print("All available commands: " + (', '.join(all_commands.keys())))
+
+
+all_commands = {
+    'build':    do_build,
+    'help':     do_help,
+    'enable':   do_enable,
+    'disable':  do_disable,
+    'list':     do_list,
     }
 
-# 
-for json_file in Path('.').glob('*/build_zip.json'):
-    try:
-        with open(json_file, 'r') as fh:
-            build_config = json.load(fh)
 
-        build_zip(
-            build_config['zip_file'],
-            json_file.parent,
-            build_config['files'],
-            config)
+def main(args):
+    # TODO: make this a config file ?
+    config = {
+        'SCAN_DIR': Path('.'),
+        'RELEASE_DIR': Path('releases'),
+        'CHECK_DIR': Path('.releases'),
+        }
 
-    except (json.decoder.JSONDecodeError, KeyError):
-        print(f"- Error parsing {json_file}")
-        continue
+    # Find all zips
+    all_configs = {}
 
+    for json_file in config['SCAN_DIR'].glob('*/build_zip.json'):
+        try:
+            with open(json_file, 'r') as fh:
+                build_config = json.load(fh)
+
+            if name_clean(build_config['zip_file']) == 'template':
+                print(f"Skipping template file {json_file}")
+                continue
+
+            config_name = build_config['zip_file'].rsplit('.', 1)[0]
+            if not 'files' in build_config:
+                raise KeyError('files')
+
+            build_config['disabled'] = (json_file.parent / '.build_disable')
+            build_config['directory'] = json_file.parent
+            all_configs[config_name] = build_config
+
+        except json.decoder.JSONDecodeError as err:
+            print(f"- Error parsing {json_file!r}: {err!r}")
+            continue
+        except KeyError as err:
+            print(f"- Missing key {err!r} in {json_file}")
+            continue
+
+    if len(args) == 1:
+        print('No commands given, assuming "build all", try "help" for commands available.')
+
+        all_commands['build'](config, all_configs, ['all'])
+        return
+
+    if args[1].lower() not in all_commands:
+        print(f'Command {args[1]!r} not found.')
+        all_commands['help'](config, all_configs, [])
+        return
+
+    all_commands[args[1].lower()](config, all_configs, args[2:])
+
+
+if __name__ == '__main__':
+    main(sys.argv)
